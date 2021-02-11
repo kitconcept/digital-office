c76418569376ba21f9bd8e581c5b4bc78ea859b3
Timo Stollenwerk <stollenwerk@kitconcept.com>
2021-02-11 21:54:49 +0100

    Add react-jitsi

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Jitsi from 'react-jitsi'
+
+const roomName = 'kitconcept-digital-office-123e4567-e89b-12d3-a456-426655440000'
+const userFullName = 'Timo Stollenwerk'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Jitsi roomName={roomName} displayName={userFullName} config={{prejoinPageEnabled: false}} />
       </header>
     </div>
   );
