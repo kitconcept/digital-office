4b0c470e67f7ed55c14b9f4fa9f63b6ce9f087aa
ThomasKman <thomas.Kindermann@yahoo.de>
2021-03-09 16:19:53 +0100

    added Avatar and KeyListener

--- src/Office/Office.js
+++ src/Office/Office.js
@@ -1,53 +1,131 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Room from "./Room.js";
+import "../App.css";
 
-class Office extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      roomName:
-        "kitconcept-digital-office-123e4567-e89b-12d3-a456-426655440000",
-      userFullName: "Timo Stollenwerk",
-    };
-  }
+const Office = () => {
+  const [AvatarSelector, setAvatarSelector] = useState(5);
+  const roomName =
+    "kitconcept-digital-office-123e4567-e89b-12d3-a456-426655440000";
+  const userFullName = "Thomas Kindermann";
+  const ArrowUp = useKeyPress("ArrowUp");
+  const ArrowDown = useKeyPress("ArrowDown");
+  const ArrowLeft = useKeyPress("ArrowLeft");
+  const ArrowRight = useKeyPress("ArrowRight");
 
-  render() {
-    return (
-      <div className="Container">
-        <div className="Floor">
-          <Room
-            RoomNumber="One"
-            roomTitle="Bonn"
-            roomName={this.state.roomName}
-            userFullName={this.state.userFullName}
-          />
-          <div className="Room Two" />
-          <Room
-            RoomNumber="Three"
-            roomTitle="Barcelona"
-            roomName={this.state.roomName}
-            userFullName={this.state.userFullName}
-          />
-          <div className="Room Four" />
-          <div className="Room Five" />
-          <div className="Room Six" />
-          <Room
-            RoomNumber="Seven"
-            roomTitle="Berlin"
-            roomName={this.state.roomName}
-            userFullName={this.state.userFullName}
-          />
-          <div className="Room Eight" />
-          <Room
-            RoomNumber="Nine"
-            roomTitle="London"
-            roomName={this.state.roomName}
-            userFullName={this.state.userFullName}
-          />
-        </div>
+  useEffect(() => {
+    if (ArrowUp) {
+      console.log("ArrowUp");
+    }
+    if (ArrowDown) {
+      console.log("ArrowDown");
+    }
+    if (ArrowRight) {
+      console.log("ArrowRight");
+    }
+    if (ArrowLeft) {
+      console.log("ArrowLeft");
+    }
+  });
+
+  return (
+    <div className="Container">
+      <div className="Floor">
+        <Room
+          RoomNumber={1}
+          roomTitle="Bonn"
+          roomName={roomName}
+          userFullName={userFullName}
+          AvatarSelector={AvatarSelector}
+        />
+        <Room
+          RoomNumber={2}
+          roomTitle=""
+          roomName=""
+          userFullName={userFullName}
+          AvatarSelector={AvatarSelector}
+        />
+        <Room
+          RoomNumber={3}
+          roomTitle="Barcelona"
+          roomName={roomName}
+          userFullName={userFullName}
+          AvatarSelector={AvatarSelector}
+        />
+        <Room
+          RoomNumber={4}
+          roomTitle=""
+          roomName=""
+          userFullName={userFullName}
+          AvatarSelector={AvatarSelector}
+        />
+        <Room
+          RoomNumber={5}
+          roomTitle=""
+          roomName=""
+          userFullName={userFullName}
+          AvatarSelector={AvatarSelector}
+        />
+        <Room
+          RoomNumber={6}
+          roomTitle=""
+          roomName=""
+          userFullName={userFullName}
+          AvatarSelector={AvatarSelector}
+        />
+        <Room
+          RoomNumber={7}
+          roomTitle="Berlin"
+          roomName={roomName}
+          userFullName={userFullName}
+          AvatarSelector={AvatarSelector}
+        />
+        <Room
+          RoomNumber={8}
+          roomTitle=""
+          roomName=""
+          userFullName={userFullName}
+          AvatarSelector={AvatarSelector}
+        />
+        <Room
+          RoomNumber={9}
+          roomTitle="London"
+          roomName={roomName}
+          userFullName={userFullName}
+          AvatarSelector={AvatarSelector}
+        />
       </div>
-    );
+    </div>
+  );
+};
+
+function useKeyPress(targetKey) {
+  const [keyPressed, setKeyPressed] = useState(false);
+
+  function downHandler({ key }) {
+    if (key === targetKey) {
+      setKeyPressed(true);
+    }
   }
+
+  const upHandler = ({ key }) => {
+    if (key === targetKey) {
+      if (key === targetKey) {
+        setKeyPressed(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", downHandler);
+    window.addEventListener("keyup", upHandler);
+
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      window.removeEventListener("keyup", upHandler);
+    };
+  }, []);
+
+  return keyPressed;
 }
 
 export default Office;
--- src/Office/Room.js
+++ src/Office/Room.js
@@ -1,19 +1,23 @@
 import React from "react";
 import CallModal from "./CallModal.js";
+import "../App.css";
+
 class Room extends React.Component {
   constructor(props) {
     super(props);
     this.handleHide = this.handleHide.bind(this);
     this.handleShow = this.handleShow.bind(this);
     this.state = {
-      RoomNumber: "Room " + this.props.RoomNumber,
+      RoomNumber: "Room Number" + this.props.RoomNumber,
       modalShow: false,
     };
   }
+
   handleShow() {
-    this.setState({
-      modalShow: true,
-    });
+    if (this.props.roomName != "")
+      this.setState({
+        modalShow: true,
+      });
   }
   handleHide() {
     this.setState({
@@ -25,7 +29,10 @@ class Room extends React.Component {
     return (
       <>
         <div className={this.state.RoomNumber} onClick={this.handleShow}>
-          {this.props.roomTitle}
+          <div className="RoomTitle">{this.props.roomTitle}</div>
+          {this.props.RoomNumber === this.props.AvatarSelector && (
+            <div className="Avatar">{this.props.userFullName.charAt(0)}</div>
+          )}
         </div>
         <CallModal
           show={this.state.modalShow}
