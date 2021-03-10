1b823518eda9c1459decbd71cfd539fc1998eb5f
ThomasKman <thomas.Kindermann@yahoo.de>
2021-03-10 11:16:32 +0100

    added call trigger on room entry

--- src/Office/Room.js
+++ src/Office/Room.js
@@ -1,55 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CallModal from "./CallModal.js";
 import "../App.css";
 
-class Room extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleHide = this.handleHide.bind(this);
-    this.handleShow = this.handleShow.bind(this);
-    this.state = {
-      RoomNumber: "Room Number" + this.props.RoomNumber,
-      modalShow: false,
-    };
-  }
+const Room = (props) => {
+  //state for showing jitsi Call in a Modal
+  const [modalShow, setModalShow] = useState(false);
+  //creating className for Room position in CSS
+  const RoomNumber = "Room Number" + props.RoomNumber;
 
-  componentDidMount() {
-    if (this.props.RoomNumber === this.props.AvatarSelector) {
-      this.handleShow();
+  //checking if the Avatar is in the current Room
+  useEffect(() => {
+    if (props.RoomNumber === props.AvatarSelector) {
+      setModalShow(true);
+    } else {
+      setModalShow(false);
     }
-  }
+  }, [props.RoomNumber, props.AvatarSelector]);
 
-  handleShow() {
-    if (this.props.roomName !== "")
-      this.setState({
-        modalShow: true,
-      });
-  }
-  handleHide() {
-    this.setState({
-      modalShow: false,
-    });
-  }
+  // return the Office room
+  return (
+    <>
+      <div className={RoomNumber}>
+        <div className="RoomTitle">{props.roomTitle}</div>
 
-  render() {
-    return (
-      <>
-        <div className={this.state.RoomNumber} onClick={this.handleShow}>
-          <div className="RoomTitle">{this.props.roomTitle}</div>
-          {this.props.RoomNumber === this.props.AvatarSelector && (
-            <div className="Avatar">{this.props.userFullName.charAt(0)}</div>
-          )}
-        </div>
-        <CallModal
-          show={this.state.modalShow}
-          onHide={this.handleHide}
-          roomTitle={this.props.roomTitle}
-          roomName={this.props.roomName}
-          userFullName={this.props.userFullName}
-        />
-      </>
-    );
-  }
-}
+        {
+          //only returning Avatar if its present in the Room
+          props.RoomNumber === props.AvatarSelector && (
+            <div className="Avatar">{props.userFullName.charAt(0)}</div>
+          )
+        }
+      </div>
+
+      {
+        //only return jitsi Call-modal if the room has a Call set up
+        props.roomName !== "" && (
+          <CallModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            roomTitle={props.roomTitle}
+            roomName={props.roomName}
+            userFullName={props.userFullName}
+          />
+        )
+      }
+    </>
+  );
+};
 
 export default Room;
