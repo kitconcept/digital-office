a44d49aa6aa5d1abe214f3ef14c5fca7ce83e516
Roberto Diaz <robertodiaz76@gmail.com>
2021-04-29 13:11:13 +0200

    apply changes to server in jenkins task

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,3 +1,7 @@
+# Backend
+(cd /srv/avatario-server/ && git fetch --all && git reset --hard origin/main)
+(cd /srv/avatario-server/ && npm install)
+(cd /srv/avatario-server/ && pm2 restart digitaloffice-server)
 # Frontend
 (cd /srv/digitaloffice.kitconcept.io/ && git fetch --all && git reset --hard origin/main)
 (cd /srv/digitaloffice.kitconcept.io/ && yarn build)
